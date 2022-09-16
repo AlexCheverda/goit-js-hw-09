c4bc88d21b8cd83541db427079e1ff808e0c2542
Aleksandr Cheverda <alexar@ua.fm>
2022-09-16 20:40:22 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,44 +1,55 @@
-import Notiflix from 'notiflix';
+// import Notiflix from 'notiflix';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const refs = {
+  form: document.querySelector('.form'),
   inputDelay: document.querySelector(`input[name='delay']`),
   inputStep: document.querySelector(`input[name='step']`),
   inputAmount: document.querySelector(`input[name='amount']`),
-  submitBtn: document.querySelector(`button`),
+  submitBtn: document.querySelector(`button[type='submit']`),
 }
 
-refs.submitBtn.addEventListener(`click`, (evt) => {
+refs.form.addEventListener('submit', onSubmitForm);
+
+  function onSubmitForm(evt) {
   evt.preventDefault();
-  let amount = Number(refs.inputAmount.value);
-  let delay = Number(refs.inputDelay.value);
-  let step = Number(refs.inputStep.value);
+  let { delay, step, amount } = onGetDate();
+
+  // let amount = Number(refs.inputAmount.value);
+  // let delay = Number(refs.inputDelay.value);
+  // let step = Number(refs.inputStep.value);
   for (let position = 1; position <= amount; position += 1) {
-    console.log('step', step);
-    console.log('delay', delay);
-    setTimeout(() => {
-      createPromise(position, delay)
-        .then(({ position, delay }) => {
-          Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-        })
-        .catch(({ position, delay }) => {
-          Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-        });
-    }, delay)
+    // console.log('step', step);
+    // console.log('delay', delay);
+    createPromise(position, delay)
+      .then(result => Notify.success(result))
+      .catch(error => Notify.failure(error));
     delay += step;
   }
-});
+  }
+
+  function onGetDate() {
+  return {
+    delay: Number(refs.inputDelay.value),
+    step: Number(refs.inputStep.value),
+    amount: Number(refs.inputAmount.value),
+  };
+}
 
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
-    if (shouldResolve) {
-      // Fulfill
-      resolve({ position, delay });
-    } else {
-      // Reject
-      reject(() => console.log(`error`));
-      console.log(delay);
-    }
+    
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        // Reject
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    }, delay);
+    
   });  
 }
+
